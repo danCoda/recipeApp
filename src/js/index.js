33ea4ca9b07fa74c5ca0154533d1a897f1b79925
Daniel Son <dson.email@gmail.com>
2020-06-28 12:51:12 +1000

    End of lecture 143. Started defining the Search model.

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,4 @@
-// Import one thing. 
-import someString from "./models/Search"; // .js isn't required. 
-console.log(`Imported default: ${someString}`);
+import Search from "./models/Search";
 
-// Import multiple things.
-import { add as addIt, multiply } from "./views/searchView";
-console.log(`Imported multiple things... Add 3, 5: ${addIt(3, 5)}`);
-console.log(`Imported multiple things... Multiply 3, 5: ${multiply(3, 5)}`);
-
-// Import everything. 
-import * as importedFunctions from "./views/searchView";
-console.log(`Imported everything... Add 3, 5: ${importedFunctions.add(3, 5)}`);
-
-//--------------------------
-
-// API calls.
-async function getRecipe(query) {
-    // const result = axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`); // axios is an npm module, but not required
-    // anymore as Chrome now supports fetch().
-    
-    try {
-        // First approach.
-        fetch(`https://forkify-api.herokuapp.com/api/search?&q=${query}`)
-        .then(result => result.json())
-        .then(data => { console.log("Result with .then:", data); });
-
-        // Second approach.
-        let result = await fetch(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        let data = await result.json();
-        console.log("Result without .then: ", data);
-    } catch (e) {
-        alert("Error: ", e);
-    }
-    
-}
-
-getRecipe("kiwifruit");
+const search = new Search("pizza");
+search.getRecipe();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,3 +1,24 @@
-// Default export, when we want to export just one thing. 
-export default "I am an exported string.";
+// Exporting a class.
 
+export default class Search {
+    constructor (query) {
+        this.query = query;
+        this.result = ""; // I added this here so we know Search's properties. 
+    }
+
+    async getRecipe() {
+        try {
+            // First approach.
+            fetch(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`)
+            .then(result => result.json())
+            .then(data => { console.log("Result with .then:", data); });
+    
+            // Second approach.
+            let result = await fetch(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = await result.json();
+            console.log("Result without .then: ", this.result);
+        } catch (e) {
+            alert("Error: ", e);
+        }
+    }
+}
