0650671fead9749715c9afd429707115a783371b
Daniel Son <dson.email@gmail.com>
2020-06-28 14:12:57 +1000

    End of lecture 144. Small change; made limitRecipeTitle(), that chops down title names so that it fits in one line in the view.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,21 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = ""; // CLear it. 
 }
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = []; // Adding stuff to an array isn't actually mutating it, so it can be a const. 
+    if (title.length > limit) {
+        title.split(" ").reduce((accumulator, current) => {
+            // Populate newTitle with words from the title, so that the number of letters don't exceed limit.
+            if (accumulator + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return accumulator + current.length; // Updated value of the accumulator.
+        }, 0);
+        return `${newTitle.join(" ")} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
     <li>
@@ -18,7 +33,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
